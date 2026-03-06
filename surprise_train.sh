9204b87f8b00080320ff9e38c4c349a0876f82af
choemj <choemj@kaist.ac.kr>
2026-03-06 13:25:30 +0900

    surprise 기법 추가

--- surprise_train.sh
+++ surprise_train.sh
@@ -1,6 +1,6 @@
 env_name=Pong
-python3 -u train.py \
-    -n "${env_name}-original-life_done-wm_2L512D8H-100k-seed1" \
+python3 -u surprise_train.py \
+    -n "${env_name}-surprise-life_done-wm_2L512D8H-100k-seed1" \
     -seed 1 \
     -config_path "config_files/STORM.yaml" \
     -env_name "ALE/${env_name}-v5" \
